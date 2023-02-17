365f2fc23dac9e1046bd38442db5ec6625e74c30
mbishu Cy <fmbishu@gmail.com>
2023-02-17 10:34:07 +0100

    update eb and add shell file for commands

---
+++ .platform/hooks/predeploy/scripts.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+#activate virtual environment
+source pipenv shell
+#makemgration( make changes)
+python manage.py makemigrations
+
+#move changes to database
+python manage.py migrate
+
+#runserver
+python manage.py runserver
