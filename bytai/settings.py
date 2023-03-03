c1ccc21706e625b9b27596a999b8a55680f8d775
mbishu Cy <fmbishu@gmail.com>
2023-03-03 10:44:52 +0100

    update eb logging

--- bytai/settings.py
+++ bytai/settings.py
@@ -232,92 +232,21 @@ JWT_AUTH = {
     #allow refreshing token
     'JWT_ALLOW_REFRESH': True
 }
-# #logging configuration
-# import logging.config
+import os
 
-
-# LOGGING_CONFIG = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#             'level': 'DEBUG',
-#             'formatter': 'verbose'
-#         }
-#     },
-#     'formatters': {
-#         'verbose': {
-#             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-#         },
-#         'simple': {
-#             'format': '%(levelname)s %(message)s'
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#             'propagate': True,
-#             'level': 'INFO',
-#         },
-#         'bytai': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#         },
-#     },
-# }
-
-# logging.config.dictConfig(LOGGING_CONFIG)
-# import os
-
-# logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'logging_config'))
-
-# # Create a logger object
-# logger = logging.getLogger(__name__)
-
-# # Example usage of logger
-# logger.debug('Debug message')
-# logger.info('Info message')
-# logger.warning('Warning message')
-# logger.error('Error message')
-# logger.critical('Critical message')
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#         'file': {
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(BASE_DIR, 'logs/django.log'),
-#             'maxBytes': 1024*1024*5, # 5 MB
-#             'backupCount': 5,
-#             'formatter': 'verbose'
-#         }
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console', 'file'],
-#             'level': 'INFO',
-#         },
-#         'bytai': {
-#             'handlers': ['console', 'file'],
-#             'level': 'DEBUG',
-#         },
-#     },
-#     'formatters': {
-#         'verbose': {
-#             'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-#             'style': '{',
-#         },
-#         'simple': {
-#             'format': '{levelname} {message}',
-#             'style': '{',
-#         },
-#     },
-# }
-
+log_level = os.getenv('LOG_LEVEL', 'INFO')
+handlers = dict(file={'class': 'logging.handlers.TimedRotatingFileHandler',
+                      'filename': os.getenv('DJANGO_LOG_FILE_PATH'),
+                      'when': 'midnight',
+                      'interval': 1,
+                      'backupCount': 1,
+                      'encoding': 'utf-8'})
+loggers = dict(django=dict(level=log_level, handlers=['file']),
+               byt=dict(level=log_level, handlers=['file']))
+LOGGING = dict(version=1,
+               disable_existing_loggers=False,
+               handlers=handlers,
+               loggers=loggers)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
